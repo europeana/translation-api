e124c22400956222e6d83eab19d1e5dfdee1750e
GordeaS <4517853+gsergiu@users.noreply.github.com>
2023-08-08 16:18:51 +0200

    move global exception handler to java configs #EA-3435

--- translation-web/src/main/java/eu/europeana/api/translation/web/exception/GlobalExceptionHandler.java
+++ translation-web/src/main/java/eu/europeana/api/translation/web/exception/GlobalExceptionHandler.java
@@ -1,14 +1,15 @@
 package eu.europeana.api.translation.web.exception;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import eu.europeana.api.commons.config.i18n.I18nService;
 import eu.europeana.api.commons.web.controller.exception.AbstractExceptionHandlingController;
+import eu.europeana.api.translation.config.TranslationBeans;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends AbstractExceptionHandlingController {
 
-	@Autowired
+	@Resource(name=TranslationBeans.BEAN_I18N_SERVICE)
 	I18nService i18nService;
 
 	protected I18nService getI18nService() {
