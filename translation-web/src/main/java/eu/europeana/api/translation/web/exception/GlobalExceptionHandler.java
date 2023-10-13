c87482aeced7cc58390b7cd517bad41e44d95446
GordeaS <4517853+gsergiu@users.noreply.github.com>
2023-10-13 10:42:44 +0200

    moved exception handler to common controller, improved error messages #EA-3567

--- translation-web/src/main/java/eu/europeana/api/translation/web/exception/GlobalExceptionHandler.java
+++ translation-web/src/main/java/eu/europeana/api/translation/web/exception/GlobalExceptionHandler.java
@@ -1,22 +1,11 @@
 package eu.europeana.api.translation.web.exception;
 
-import javax.servlet.http.HttpServletRequest;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.NoHandlerFoundException;
 import eu.europeana.api.commons.config.i18n.I18nService;
-import eu.europeana.api.commons.error.EuropeanaApiErrorResponse;
-import eu.europeana.api.commons.error.EuropeanaApiException;
-import eu.europeana.api.commons.error.EuropeanaI18nApiException;
 import eu.europeana.api.commons.web.exception.EuropeanaGlobalExceptionHandler;
-import eu.europeana.api.commons.web.exception.HttpException;
 import eu.europeana.api.translation.config.BeanNames;
 import eu.europeana.api.translation.web.service.RequestPathMethodService;
 
@@ -37,90 +26,9 @@ public class GlobalExceptionHandler extends EuropeanaGlobalExceptionHandler {
     this.requestPathMethodService = requestPathMethodService;
     this.i18nService = i18nService;
   }
-  
-  protected I18nService getI18nService() {
+
+  public I18nService getI18nService() {
     return i18nService;
-  }
-
-  
-  /**
-   * Default handler for EuropeanaI18nApiException types
-   *
-   * @param e caught exception
-   * @param httpRequest the http request
-   * @return api response entity
-   */
-  @ExceptionHandler(EuropeanaI18nApiException.class)
-  public ResponseEntity<EuropeanaApiErrorResponse> handleEuropeanaApiException(
-      EuropeanaI18nApiException e, HttpServletRequest httpRequest) {
-
-    return buildApiErrorResponse(e, httpRequest, e.getI18nKey(), e.getI18nParams());
-  }
-
-  private ResponseEntity<EuropeanaApiErrorResponse> buildApiErrorResponse(EuropeanaApiException e,
-      HttpServletRequest httpRequest, String i18nKey, String[] i18NParams) {
-    EuropeanaApiErrorResponse response =
-        new EuropeanaApiErrorResponse.Builder(httpRequest, e, stackTraceEnabled())
-            .setStatus(e.getResponseStatus().value())
-            .setError(e.getResponseStatus().getReasonPhrase())
-            .setMessage(e.doExposeMessage() ? buildResponseMessage(e, i18nKey, i18NParams) : null)
-            // code only included in JSON if a value is set in exception
-            .setCode(e.getErrorCode())
-            .setSeeAlso(getSeeAlso())
-            .build();
-    return ResponseEntity.status(e.getResponseStatus()).headers(createHttpHeaders(httpRequest))
-        .body(response);
-  }
-  
-  private String buildResponseMessage(Exception e, String i18nKey, String[] i18nParams) {
-    if (StringUtils.isBlank(i18nKey)) {
-      return e.getMessage();
-    } else {
-      return i18nService.getMessage(i18nKey, i18nParams);
-    }
-  }
-
-
-  /**
-   * Default handler for EuropeanaI18nApiException types
-   *
-   * @param e caught exception
-   * @param httpRequest the http request
-   * @return api response entity
-   */
-  @ExceptionHandler(HttpException.class)
-  public ResponseEntity<EuropeanaApiErrorResponse> handleCommonHttpException(
-      HttpException e, HttpServletRequest httpRequest) {
-    EuropeanaApiErrorResponse response =
-        new EuropeanaApiErrorResponse.Builder(httpRequest, e, stackTraceEnabled())
-            .setStatus(e.getStatus().value())
-            .setError(e.getStatus().getReasonPhrase())
-            .setMessage( i18nService.getMessage(e.getI18nKey(), e.getI18nParams()))
-            // code only included in JSON if a value is set in exception
-            .setCode(e.getI18nKey())
-            .setSeeAlso(getSeeAlso())
-            .build();
-    return ResponseEntity.status(e.getStatus()).headers(createHttpHeaders(httpRequest))
-        .body(response);
-  }
-
-  /**
-   * mapping for resource not found errors
-   * @param e exception to handle
-   * @param httpRequest the http request
-   */
-  @ExceptionHandler(NoHandlerFoundException.class)
-  public ResponseEntity<EuropeanaApiErrorResponse> handleNoHandlerFoundException(
-      NoHandlerFoundException e, HttpServletRequest httpRequest) {
-    EuropeanaApiErrorResponse response =
-        new EuropeanaApiErrorResponse.Builder(httpRequest, e, stackTraceEnabled())
-            .setStatus(HttpStatus.NOT_FOUND.value())
-            .setError(HttpStatus.NOT_FOUND.getReasonPhrase())
-            .setMessage(e.getMessage())
-            .setSeeAlso(getSeeAlso())
-            .build();
-    return ResponseEntity.status(HttpStatus.NOT_FOUND.value())
-        .contentType(MediaType.APPLICATION_JSON).body(response);
   }
 
 }
