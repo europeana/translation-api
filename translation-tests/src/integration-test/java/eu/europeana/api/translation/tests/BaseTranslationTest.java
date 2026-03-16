b1ed7a56ea80c83f4698e27942d1693a85bab162
gsergiu <4517853+gsergiu@users.noreply.github.com>
2026-03-16 16:56:13 +0100

    upgrade redis version, fix code quality

--- translation-tests/src/integration-test/java/eu/europeana/api/translation/tests/BaseTranslationTest.java
+++ translation-tests/src/integration-test/java/eu/europeana/api/translation/tests/BaseTranslationTest.java
@@ -61,15 +61,15 @@ public abstract class BaseTranslationTest extends IntegrationTestUtils {
   /** Maps Metis dereferenciation URIs to mocked XML responses */
   public static final Map<String, String> LANG_DETECT_RESPONSE_MAP = initLanguageDetectMap();
   public static final Map<String, String> TRANSLATION_RESPONSE_MAP = initTranslationMap();
-
+  public static final String REDIS_IMAGE_NAME = "redis:8-alpine";
+  
   //start redis test container
   protected final static int redisContainerPort=6379;
-  private static GenericContainer<?> REDIS_CONTAINER = startRedisTestContainer();
+  private static GenericContainer<?> redisContainer = startRedisTestContainer();
   private static GenericContainer<?> startRedisTestContainer() {
     @SuppressWarnings("resource")
     GenericContainer<?> redis = 
-      new GenericContainer<>(DockerImageName.parse("redis:5.0.3-alpine")).withExposedPorts(redisContainerPort);
-      //new GenericContainer<>(DockerImageName.parse("redis:8-alpine")).withExposedPorts(redisContainerPort);
+      new GenericContainer<>(DockerImageName.parse(REDIS_IMAGE_NAME)).withExposedPorts(redisContainerPort);
     redis.start();
     return redis;
   }
@@ -153,7 +153,7 @@ public abstract class BaseTranslationTest extends IntegrationTestUtils {
 
     registry.add("translation.google.projectId", () -> "project-id-test");
     registry.add("translation.google.usehttpclient", () -> "true");
-    registry.add("redis.connection.url", () -> "redis://"+ REDIS_CONTAINER.getHost() + ":" + REDIS_CONTAINER.getMappedPort(redisContainerPort) + "/");
+    registry.add("redis.connection.url", () -> "redis://"+ redisContainer.getHost() + ":" + redisContainer.getMappedPort(redisContainerPort) + "/");
     registry.add("translation.eTranslation.baseUrl", () -> ETranslationTranslationService.FAKE_BASE_URL_FOR_TESTING);
     registry.add("translation.eTranslation.credentials", () -> "");
     registry.add("translation.eTranslation.truncate", () -> false);
